4fbe4f82720b7a734cc5ee040f319a1877b69836
Cleyviton <cleyvitonkeke@gmail.com>
2023-10-13 09:29:46 -0300

    feat(tasks): create task model/table

---
+++ src/main/java/br/com/cleyvitonferreira/todolist/task/TaskModel.java
@@ -0,0 +1,35 @@
+package br.com.cleyvitonferreira.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+
+  @Column(length = 50)
+  private String title;
+
+  private String description;
+
+  private LocalDateTime startAt;
+
+  private LocalDateTime endAt;
+
+  private String priority;
+  
+  private LocalDateTime createdAt;
+
+  private UUID userId;
+
+};
